eb6c5e2b0e925251ea3019b6850dd02c5fac33b2
DataLinhNguyen <107485552+DataLinhNguyen@users.noreply.github.com>
2022-10-20 08:47:41 +0700

    initial commit

---
+++ ch06_ex1_email/src/java/murach/email/EmailListServlet.java
@@ -0,0 +1,73 @@
+package murach.email;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import murach.business.User;
+import murach.data.UserDB;
+
+public class EmailListServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = "/index.html";
+        
+        // initialize the current year that's used in the copyright notice
+        GregorianCalendar currentDate = new GregorianCalendar();
+        int currentYear = currentDate.get(Calendar.YEAR);
+        request.setAttribute("currentYear", currentYear);
+        
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.jsp";    // the "join" page
+        } 
+        else if (action.equals("add")) {
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+            //display currentyear scriplet from footer.jsp
+            GregorianCalendar currentDate1 =new GregorianCalendar();
+         	int currentYear1 =currentDate1.get(Calendar.YEAR);
+            // store data in User object
+            User user = new User(firstName, lastName, email);
+
+            // validate the parameters
+            String message;
+            if (firstName == null || lastName == null || email == null ||
+                firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+                message = "Please fill out all three text boxes.";
+                url = "/index.jsp";
+            } 
+            else {
+                message = null;
+                url = "/thanks.jsp";
+                UserDB.insert(user);
+            }
+            request.setAttribute("user", user);
+            request.setAttribute("message", message);
+            //set year as an attribute
+            request.setAttribute("currentYear", currentYear1);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }    
+}
