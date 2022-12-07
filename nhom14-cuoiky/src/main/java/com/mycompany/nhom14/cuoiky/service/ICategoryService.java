02909a5c615669eaaa3a152a67207ab5107edd62
DataLinhNguyen <107485552+DataLinhNguyen@users.noreply.github.com>
2022-12-07 22:07:27 +0700

    delete

--- nhom14-cuoiky/src/main/java/com/mycompany/nhom14/cuoiky/service/ICategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.nhom14.cuoiky.service;
-
-import com.mycompany.nhom14.cuoiky.entities.Category;
-
-import java.util.List;
-
-public interface ICategoryService {
-    List<Category> getAll();
-}
