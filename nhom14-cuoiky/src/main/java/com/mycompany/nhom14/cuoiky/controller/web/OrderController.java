baae376bd9077d172a555b9cdc6fa69851317f90
DataLinhNguyen <107485552+DataLinhNguyen@users.noreply.github.com>
2022-12-08 09:07:42 +0700

    Mặc định login user 5

--- nhom14-cuoiky/src/main/java/com/mycompany/nhom14/cuoiky/controller/web/OrderController.java
+++ nhom14-cuoiky/src/main/java/com/mycompany/nhom14/cuoiky/controller/web/OrderController.java
@@ -15,6 +15,7 @@ import com.mycompany.nhom14.cuoiky.service.impl.CartItemServiceImpl;
 import com.mycompany.nhom14.cuoiky.service.impl.OrderServiceImpl;
 import com.mycompany.nhom14.cuoiky.service.impl.UserServiceImpl;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Date;
 import javax.servlet.RequestDispatcher;
@@ -73,10 +74,27 @@ public class OrderController extends HttpServlet {
         int total = user.getCart().getTotal();
         if (total > 0) {
             String fullName = request.getParameter("fullName");
+            byte[] temp1 = fullName.getBytes(StandardCharsets.ISO_8859_1);
+            fullName = new String(temp1, StandardCharsets.UTF_8);
+
             String phoneNumber = request.getParameter("phoneNumber");
-            String email = request.getParameter("email");
+            byte[] temp2 = phoneNumber.getBytes(StandardCharsets.ISO_8859_1);
+            phoneNumber = new String(temp2, StandardCharsets.UTF_8);
+
             String note = request.getParameter("note");
+            byte[] temp3 = note.getBytes(StandardCharsets.ISO_8859_1);
+            note = new String(temp3, StandardCharsets.UTF_8);
+
             String address = request.getParameter("address");
+            byte[] temp4 = address.getBytes(StandardCharsets.ISO_8859_1);
+            address = new String(temp4, StandardCharsets.UTF_8);
+
+            String email = request.getParameter("email");
+            System.err.println(fullName);
+            System.err.println(phoneNumber);
+            System.err.println(email);
+            System.err.println(note);
+            System.err.println(address);
             Date date = new Date(System.currentTimeMillis());
             Order order = new Order();
             order.setAddress(address);
